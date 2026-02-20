4ed5249ad968cb5bc817fcae768bbfa848ae35d2
GANGINENI SAHITH KRISHNA <148518981+sahith-krishna19@users.noreply.github.com>
2026-02-20 19:06:22 +0530

    Add onboarding SQL: MI_ACCESS_CONTROL.sql

---
+++ onboarding/MI_ACCESS_CONTROL.sql
@@ -0,0 +1,10 @@
+-- BCBSA Plan Onboarding SQL
+-- Generated: 2026-02-20 19:06:20
+-- Total Statements: 4
+
+
+-- ========== MI_ACCESS_CONTROL ==========
+INSERT INTO MI_ACCESS_CONTROL (BHI_HOME_PLAN_ID_CD, LOCN_CD, BHI_HOME_PLAN_NM, LOCN_NM, BHI_HLDNG_CMPNY_CD, BHI_HLDNG_CMPNY_NM, ANTHEM_PORTAL_IND) VALUES ('G15', 'bcbsa-chg', 'NHIT', 'Trivandrum', 'LH032', 'National Health Insurance Trivandrum', 'N');
+INSERT INTO MI_ACCESS_CONTROL (BHI_HOME_PLAN_ID_CD, LOCN_CD, BHI_HOME_PLAN_NM, LOCN_NM, BHI_HLDNG_CMPNY_CD, BHI_HLDNG_CMPNY_NM, ANTHEM_PORTAL_IND) VALUES ('G15', 'bhi-chg', 'NHIT', 'Trivandrum', 'LH032', 'National Health Insurance Trivandrum', 'N');
+INSERT INTO MI_ACCESS_CONTROL (BHI_HOME_PLAN_ID_CD, LOCN_CD, BHI_HOME_PLAN_NM, LOCN_NM, BHI_HLDNG_CMPNY_CD, BHI_HLDNG_CMPNY_NM, ANTHEM_PORTAL_IND) VALUES ('G15', 'bcbs-nc', 'National Health Insurance Trivandrum', 'Kochin', 'LH032', 'National Health Insurance Trivandrum', 'N');
+INSERT INTO MI_ACCESS_CONTROL (BHI_HOME_PLAN_ID_CD, LOCN_CD, BHI_HOME_PLAN_NM, LOCN_NM, BHI_HLDNG_CMPNY_CD, BHI_HLDNG_CMPNY_NM, ANTHEM_PORTAL_IND) VALUES ('G15', 'nc-found', 'National Health Insurance Trivandrum', 'Kochin', 'LH032', 'National Health Insurance Trivandrum', 'N');
