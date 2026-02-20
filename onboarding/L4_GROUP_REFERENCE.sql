886c2490bbd228c1e755993e6ab13950b55136c1
GANGINENI SAHITH KRISHNA <148518981+sahith-krishna19@users.noreply.github.com>
2026-02-20 19:05:38 +0530

    Add onboarding SQL: L4_GROUP_REFERENCE.sql

---
+++ onboarding/L4_GROUP_REFERENCE.sql
@@ -0,0 +1,7 @@
+-- BCBSA Plan Onboarding SQL
+-- Generated: 2026-02-20 19:05:36
+-- Total Statements: 1
+
+
+-- ========== L4_GROUP_REFERENCE ==========
+INSERT INTO L4_GROUP_REFERENCE (L4_GRP_PLAN_ID_CD, NM, NOTIF_EMAIL_ADDR, EFF_DT, EXPRTN_DT, CRT_TS, CRT_USER_CD, UPDT_TS, UPDT_USER_CD) VALUES ('G01', 'National Health Insurance Trivandrum', 'Operationsteam@G01.com', '2008-01-01 00:00:00', '9999-12-31 00:00:00', NULL, NULL, NULL, NULL);
